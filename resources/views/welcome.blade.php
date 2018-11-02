d9153df3b148bbf0a6f1faf730a1fabab33b9fb3
David Fogg <vanwhebin@gmail.com>
2018-11-02 16:11:32 +0800

    add laravel-admin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,11 @@
 <html>
     <head>
         <title>Laravel</title>
+        <script type="text/javascript">
+            (function(d, src, c) { var t=d.scripts[d.scripts.length - 1],s=d.createElement('script');s.id='la_x2s6df8d';s.async=true;s.src=src;s.onload=s.onreadystatechange=function(){var rs=this.readyState;if(rs&&(rs!='complete')&&(rs!='loaded')){return;}c(this);};t.parentElement.insertBefore(s,t.nextSibling);})(document,
+                'https://partu.ladesk.com/scripts/track.js',
+                function(e){ LiveAgent.createButton('3d2ee213', e); });
+        </script>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
